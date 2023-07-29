120241dc195daa5ef79fe3537d785099eaaa18bd
Damir <damirr765@gmail.com>
2023-07-29 23:28:16 +0300

    add: Post and Category model

--- blog/models.py
+++ blog/models.py
@@ -1,1 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=50)
+    text = models.TextField(max_length=250)
+    posted_date = models.DateField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    tag = models.ManyToManyField(Category)
+
+    def __str__(self):
+        return self.title
--- blog/views.py
+++ blog/views.py
@@ -1,72 +1,49 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.templatetags.static import static
-
-posts = [
-    {
-        'title': "Microsoft kinda nailed it with this one",
-        'text': "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-        'author': "Microsoft"
-    },
-    {
-        'title': "Some boring title",
-        'text': "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-        'author': "Google"
-    },
-    {
-        'title': "Hello World",
-        'text': "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-        'author': "Apple"
-    },
-    {
-        'title': "Hello World",
-        'text': "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-        'author': "Apple"
-    },
-    {
-        'title': "Hello World",
-        'text': "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-        'author': "Apple"
-    },
-    {
-        'title': "Hello World",
-        'text': "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-        'author': "Apple"
-    },
-    {
-        'title': "Hello World",
-        'text': "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-        'author': "Apple"
-    },
-]
-categories = [
-    {
-        'title': 'All',
-        'icon': static('blog/icons/hash-outline.svg')
-    },
-    {
-        'title': 'Popular',
-        'icon': static('blog/icons/fire-outline.svg')
-    },
-    {
-        'title': 'Design',
-        'icon': static('blog/icons/color-palette-outline.svg')
-    },
-    {
-        'title': 'Development',
-        'icon': static('blog/icons/code-outline.svg')
-    },
-    {
-        'title': 'Marketing',
-        'icon': static('blog/icons/message-circle-outline.svg')
-    },
-]
+from .models import Post, Category
 
 
 def home(request):
+    categories = [
+        {
+            'title': 'All',
+            'icon': static('blog/icons/hash-outline.svg'),
+            'category': Category.objects.filter(name="default").first()
+        },
+        {
+            'title': 'Popular',
+            'icon': static('blog/icons/fire-outline.svg'),
+            'category': Category.objects.filter(name="default").first()
+        },
+        {
+            'title': 'Design',
+            'icon': static('blog/icons/color-palette-outline.svg'),
+            'category': Category.objects.filter(name="design").first()
+        },
+        {
+            'title': 'Development',
+            'icon': static('blog/icons/code-outline.svg'),
+            'category': Category.objects.filter(name="development").first()
+        },
+        {
+            'title': 'Marketing',
+            'icon': static('blog/icons/message-circle-outline.svg'),
+            'category': Category.objects.filter(name="marketing").first()
+        },
+    ]
+    category = request.GET.get('category')
+
+    if not category:
+        posts = Post.objects.all()
+    else:
+        posts = Category.objects.filter(name=category).first().post_set.all()
+
+
     context = {
         'posts': posts,
-        'categories': categories
+        'categories': categories,
+        'category': category
     }
 
     return render(request, 'blog/home.html', context)
