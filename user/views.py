f4f4ec93805001f269794c39e65c520a0961d0e4
Damir <damirr765@gmail.com>
2023-07-30 16:36:00 +0300

    add: user login flow

--- user/views.py
+++ user/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
 from .forms import UserRegisterForm
 
 
@@ -27,11 +28,23 @@ def register(request):
 
 
 def login(request):
-    if request.method != 'POST':
-        context = {
-            'title': 'Login'
-        }
-        return render(request, 'user/login.html', context)
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            auth_login(request, user)
+            return redirect('blog/feed')
+        else:
+            messages.error(
+                'User registration error, check password or username')
 
-    else:
-        print("login handle")
+    context = {
+        'title': 'Login'
+    }
+    return render(request, 'user/login.html', context)
+
+
+def logout(request):
+    auth_logout(request)
+    return redirect('blog/feed')
